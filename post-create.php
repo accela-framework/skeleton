3cb37b087f6f34d0417158996569b5afa05e59c4
green spot <in.green.spot@gmail.com>
2025-12-08 09:29:41 +0900

    update

---
+++ post-create.php
@@ -0,0 +1,11 @@
+<?php
+
+$file = __DIR__ . "/composer.json";
+$json = json_decode(file_get_contents($file), true);
+
+$json["name"] = basename(dirname(__DIR__));
+unset($json["description"]);
+
+file_put_contents($file,json_encode($json, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT) . PHP_EOL);
+
+unlink(__FILE__);
