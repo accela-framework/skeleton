2a287ff73684f1a66d468c7286b64bfe5f579872
green spot <in.green.spot@gmail.com>
2025-12-08 10:25:59 +0900

    update

--- post-create.php
+++ post-create.php
@@ -5,6 +5,7 @@ $json = json_decode(file_get_contents($file), true);
 
 $json["name"] = basename(dirname(__DIR__));
 unset($json["description"]);
+unset($json["scripts"]);
 
 file_put_contents($file,json_encode($json, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT) . PHP_EOL);
 
