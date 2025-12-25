e28f719c8a3ede350ab89e760d8bfcc7cbb7ac40
green spot <in.green.spot@gmail.com>
2025-12-25 16:30:51 +0900

    fix

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 
-namespace Accela;
+require_once __DIR__ . "/vendor/autoload.php";
 
 use Accela\Env;
 Env::load(__DIR__ . "/env.php");
-
-require_once __DIR__ . "/vendor/autoload.php";
 
 $accela = require_once __DIR__ . "/app/init-accela.php";
 $accela->route(isset($_SERVER["PATH_INFO"]) ? $_SERVER["PATH_INFO"] : "/");
